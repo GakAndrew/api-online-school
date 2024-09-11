0a31170a54c12e84c5273e841d3d828602600ae2
Andrew_G <81982187+GakAndrew@users.noreply.github.com>
2024-09-11 19:49:39 +0300

    add git ignor

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37304d54c7f0ffc36912557ec5dfb909f45bd598")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+652fdc741723aa0baeda1ac9b0961abba484d815")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
