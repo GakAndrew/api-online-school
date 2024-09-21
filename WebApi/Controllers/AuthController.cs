3585cdb7e49092cabf5389e111ce36aec8a350b5
Daniil <danila_emelev@mail.ru>
2024-09-21 16:00:12 +0300

    Добавление jwtToken

---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using OnlineSchool.Core;
+using OnlineSchool.Core.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using OnlineSchool.Application.DTOs;
+using BCrypt.Net;
+using OnlineSchool.Application.Services;
+
+namespace OnlineSchool.WebApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly OnlineSchoolDbContext _context;
+        private readonly JwtService _jwtService;
+
+        public AuthController(IConfiguration configuration, OnlineSchoolDbContext context, JwtService jwtService)
+        {
+            _configuration = configuration;
+            _context = context;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register([FromBody] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+            if (existingUser != null)
+            {
+                return BadRequest("Пользователь с таким адресом электронной почты уже существует.");
+            }
+
+            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+            user.CreatedAt = DateTime.UtcNow;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Пользователь успешно зарегистрирован" });
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(model.Password, user.Password))
+            {
+                return Unauthorized("Неверный адрес электронной почты или пароль");
+            }
+
+            var token = _jwtService.GenerateToken(user.Id.ToString(), user.Email);
+
+            return Ok(new { token });
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using OnlineSchool.Application.Services;
 using OnlineSchool.Core;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -16,6 +20,25 @@ builder.Services.AddDbContext<OnlineSchoolDbContext>(options =>
     options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = configuration["Jwt:Issuer"],
+            ValidAudience = configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"])),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddScoped<JwtService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,8 +48,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
